e33f8409b56d02d6c5d7389bc41429bcc612eb70
McHektor123 <44132381+McHektor123@users.noreply.github.com>
2020-01-10 12:56:57 +0100

    Update Row.h

--- Row.h
+++ Row.h
@@ -21,47 +21,4 @@ private:
 	string m_gerne;
 	int m_rating;
 };
-	//Iterator for the Rows
-	struct It
-		{
-		Row* vec_;
-		int pointer_;
-
-		It(Row* vec) : vec_{ vec }, pointer_{ 0 } {}
-
-		It(Row* vec, int size) : vec_{ vec }, pointer_{ size } {}
-
-		bool operator!=(const It other) const
-		{
-			return !(*this == other);
-		}
-
-		bool operator==(const It other) const
-		{
-			return pointer_ == other.pointer_;
-		}
-
-		It operator++()
-		{
-			++pointer_;
-			return *this;
-		}
-
-		It operator--()
-		{
-			--pointer_;
-			return *this;
-		}
-
-		It operator++(int)
-		{
-			It tmp(*this);
-			++pointer_;
-			return tmp;
-		}
-
-		Row* operator*() 
-		{
-			return vec_;
-		}
-	};
+	
